368650cb669b31afa621726c6118c111c193668b
bwalters13 <34872925+bwalters13@users.noreply.github.com>
2023-12-22 09:43:04 -0800

    dropdown instead of tabs

--- src/Stats.js
+++ src/Stats.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MyNavbar from './Navbar.js';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import './Stats.css';
 
 export default function Stats({}) {
+    const [plot, setPlot] = useState('troll_plot');
+    const [title, setTitle] = useState('Spencer was the most trolled plot');
+    let handleClick = (e) => {
+        console.log(e);
+        setPlot(e.target.id);
+        setTitle(e.target.innerText);    
+    }
+
+    let openFullscreen = () => {
+        document.getElementById('plot')?.requestFullscreen();
+    }    
     return (
         <div>
             <MyNavbar/>
-            <Tabs defaultActiveKey="troll" className="mb-3" fill={true} variant='pills' style={{paddingTop: '2%'}}>
+            {/* <Tabs defaultActiveKey="troll" className="mb-3" fill={true} variant='pills' style={{paddingTop: '2%'}}>
                 <Tab eventKey="troll" title="Spencer was the most trolled plot" className='troll'>
                     <div>
                         <img src="/img/troll_plot.png" className="plot"></img>
@@ -40,7 +51,19 @@ export default function Stats({}) {
                     <img src="/img/plays.png" className="plot"></img>
                 </Tab>
 
-            </Tabs>
+            </Tabs> */}
+            <DropdownButton id="dropdown-item-button" title={title}>
+                <Dropdown.Item id="troll_plot" as="button" onClick={handleClick}>Spencer was the most trolled plot</Dropdown.Item>
+                <Dropdown.Item id="troll_index" as="button" onClick={handleClick}>Spencer was the most trolled</Dropdown.Item>
+                <Dropdown.Item id="quarters" as="button" onClick={handleClick}>Quarters Played By Round</Dropdown.Item>
+                <Dropdown.Item id="drives" as="button" onClick={handleClick}>Drives Played By Round</Dropdown.Item>
+                <Dropdown.Item id="plays" as="button" onClick={handleClick}>Plays Played By Round</Dropdown.Item>
+                <Dropdown.Item id="opt_standings" as="button" onClick={handleClick}>Best Ball Standings</Dropdown.Item>
+                <Dropdown.Item id="no_kicker" as="button" onClick={handleClick}>Standings w/ No Kickers</Dropdown.Item>
+                <Dropdown.Item id="inactives" as="button" onClick={handleClick}>Most Inactives</Dropdown.Item>
+                <Dropdown.Item id="draft" as="button" onClick={handleClick}>Points By Drafted Players</Dropdown.Item>
+            </DropdownButton>
+            <img src={"/img/" + plot + ".png"} className="plot" onClick={openFullscreen}></img>
         </div>
     )
 }
